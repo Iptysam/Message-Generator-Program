0115c6572adbbbd1b44abf688d4433bdd18fe4fb
Iptysam <mukadamiptisam@gmail.com>
2023-07-06 10:28:36 +0300

    Update files

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-function generateRandomNumber(num) {
-    // Gets # from 0 -> num - 1
-    return Math.floor(Math.random() * num)
-  }
-  
-  const collectiveWisdom = {
-    signInfo: ['star', 'moon', 'sun', 'comet'],
-    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
-    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
-  }
-  
-  // Store the 'wisdom' in an array
-  let personalWisdom = []
-  
-  // Iterate over the object
-  for(let prop in collectiveWisdom) {
-    let optionIdx = generateRandomNumber(collectiveWisdom[prop].length)
-  
-    // use the object's properties to customize the message being added to personalWisdom  
-    switch(prop) {
-      case 'signInfo':
-        personalWisdom.push(`Your sign right now is a "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'fortuneOutput':
-        personalWisdom.push(`You are having: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'advice':
-        personalWisdom.push(`You should: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      default:
-        personalWisdom.push('There is not enough info.')
-    }
-  }
-  
-  function formatWisdom(wisdom) {
-    // Add some ASCII here?
-    // Add in more symbols to the array? like emojies or what not?
-    const formatted = personalWisdom.join('\n')
-    console.log(formatted)
-  }
-  
-  formatWisdom(personalWisdom);
+const Randomize = () => {
+  let messages = ['Today I will do what others won\'t, so tomorrow I can accomplish what others can\'t - Jerry Rice',
+      'Precision beats power and timing beats speed - Conor McGregor',
+      'You have time - Gary Vaynerchuk'
+  ];
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  let randomMsg = Math.floor(Math.random() * messages.length);
+  let randomNum = Math.floor(Math.random() * numbers.length);
+
+  let output = [];
+  output.push('Your motivational quote is: ' + messages[randomMsg] + ' and your number is: ' + numbers[randomNum]);
+  return output
+};
+
+console.log(Randomize());
